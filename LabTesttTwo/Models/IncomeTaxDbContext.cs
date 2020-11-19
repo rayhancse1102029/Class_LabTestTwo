b9d0c1e9d541c3cc85328c990948190f686b4b0e
rayhancse15git <rayhancse15@gmail.com>
2020-11-19 09:52:12 +0600

    weer

---
+++ LabTesttTwo/Models/IncomeTaxDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using LabTesttTwo.Data.Entity;
+using LabTesttTwo.Models;
+
+namespace LabTesttTwo.Data
+{
+    public class IncomeTaxDbContext : IdentityDbContext<ApplicationUser>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public IncomeTaxDbContext(DbContextOptions<IncomeTaxDbContext> options, IHttpContextAccessor _httpContextAccessor) : base(options)
+        {
+            this._httpContextAccessor = _httpContextAccessor;
+        }
+              
+
+        #region Income Tax
+
+        public DbSet<IncomeTax> IncomeTaxes { get; set; }
+        
+        #endregion
+    }
+}
